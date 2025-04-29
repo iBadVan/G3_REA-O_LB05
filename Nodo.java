cc25e5143a4054c4a7aa6e516892f935ba7dd498
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-29 16:28:12 -0500

    Imprlementacion de constructor y setyget de contenido

--- Nodo.java
+++ Nodo.java
@@ -2,4 +2,22 @@ public class Nodo<T>{
     public T contenido;
     public Nodo<T> siguienteNodo;  
 
+    public Nodo( T cont, Nodo<T> siguienteNodo){
+        this.contenido=cont;
+        this.siguienteNodo=siguienteNodo;
+    }
+
+    public void setcontenido(T nuevoContenido){
+        this.contenido=nuevoContenido;
+    }
+
+    public T getContenido(){
+        return this.contenido;
+    }
+
+
+    
+
+
+
 }
