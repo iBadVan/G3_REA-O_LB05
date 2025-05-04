d1ce1ee12bd3d318af105ff9f09ddf820f5d8701
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-04 17:31:27 -0500

    agregar una tarea, se recorre hasta buscar el nodo final, y a ese nodo se le agrega como parametro de siguiente nodo el nodo a agregar

--- Actividad/GestorDeTareas.java
+++ Actividad/GestorDeTareas.java
@@ -6,6 +6,20 @@ public class GestorDeTareas <T> {
     public GestorDeTareas(){
         cabeza=null;
     }
+
+    public void agregarTarea(T tarea){
+        Nodo <T> nodoAAgregar = new Nodo<T>(tarea,null);
+        if (cabeza==null){
+            cabeza=nodoAAgregar;
+        } else {
+            Nodo <T> nodoFinal=cabeza;
+            while (nodoFinal.getSiguienteNodo()!=null) {
+                nodoFinal=nodoFinal.getSiguienteNodo();                
+            }
+            nodoFinal.setSiguienteNodo(nodoAAgregar);
+        }
+    }
+
     
 
 }
